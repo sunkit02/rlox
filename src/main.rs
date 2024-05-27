aad164cba7cb3aa450a8016bf2984159c710a4eb
Sun Kit Tsui <sunkittsui@gmail.com>
2024-05-27 10:42:16 -0400

    feat: Integrate interpreter into REPL and file execution

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,26 @@ use std::{
     str::FromStr,
 };
 
-use rlox::lexer::{self, Lexer};
-use rlox::parser::Parser;
+use rlox::{
+    interpreter::{ErrorReporter, Interpreter},
+    lexer::{self, Lexer},
+};
+use rlox::{lexer::token::Token, parser::Parser};
+
+struct StderrErrorReporter;
+
+impl ErrorReporter for StderrErrorReporter {
+    fn report_err(&self, error: &rlox::interpreter::error::RuntimeError) {
+        eprintln!("{error}");
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     // Skip the current exe name
     let args: Vec<String> = env::args().skip(1).collect();
+
+    let err_reporter: Box<dyn ErrorReporter> = Box::new(StderrErrorReporter);
+
     if args.len() > 1 {
         println!("Usage: rlox [script]");
         process::exit(64);
@@ -20,7 +34,7 @@ fn main() -> anyhow::Result<()> {
         let path = PathBuf::from_str(&args[0]).context("convert String to PathBuf")?;
         run_file(path)?;
     } else {
-        run_prompt()?;
+        run_prompt([err_reporter])?;
     };
 
     Ok(())
@@ -29,12 +43,19 @@ fn main() -> anyhow::Result<()> {
 fn run_file(path: PathBuf) -> anyhow::Result<()> {
     let src_file = fs::read_to_string(path)?;
 
-    run(src_file.as_str())?;
+    let err_reporter: Box<dyn ErrorReporter> = Box::new(StderrErrorReporter);
+    let mut interpreter = Interpreter::with_reporters([err_reporter]);
+
+    run(src_file.as_str(), &mut interpreter)?;
 
     Ok(())
 }
 
-fn run_prompt() -> anyhow::Result<()> {
+fn run_prompt<I: IntoIterator<Item = Box<dyn ErrorReporter>>>(
+    err_reporter: I,
+) -> anyhow::Result<()> {
+    let mut interpreter = Interpreter::with_reporters(err_reporter);
+
     let prompt: &str = "> ";
 
     print!("{}", prompt);
@@ -42,7 +63,7 @@ fn run_prompt() -> anyhow::Result<()> {
     for line in stdin().lines() {
         let line = line.context("read line from stdin")?;
 
-        if let Err(e) = run(line.as_str()) {
+        if let Err(e) = run(line.as_str(), &mut interpreter) {
             eprintln!("{e}");
         }
 
@@ -53,18 +74,18 @@ fn run_prompt() -> anyhow::Result<()> {
     Ok(())
 }
 
-fn run(source: &str) -> anyhow::Result<()> {
+fn run(source: &str, interpreter: &mut Interpreter) -> anyhow::Result<()> {
     let lexer = Lexer::new(&source);
     let tokens = lexer
         .scan_all_tokens()
         .into_iter()
-        .collect::<lexer::Result<Vec<_>>>()?;
+        .collect::<lexer::Result<Vec<Token>>>()?;
     let mut parser = Parser::new(tokens);
     let statements = parser.parse()?;
 
-    for stmt in statements {
-        println!("{stmt}");
-    }
+    statements.iter().for_each(|stmt| println!("{stmt}"));
+
+    interpreter.interpret(statements);
 
     Ok(())
 }
